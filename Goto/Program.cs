82af26b1b8ece391c8dcace5af144844228ad6ac
hallamw <58642499+hallamw@users.noreply.github.com>
2021-05-18 17:48:04 +1200

    Add files via upload

---
+++ Goto/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Goto{   
+     
+    class Program    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Checkers *****");
+            int[,] checkers = new int[4, 4];
+
+            int y = 0;
+            bool squareValue = true;
+
+        PopulateChecker:
+
+            checkers[y, 0] = Convert.ToInt32(squareValue);
+            checkers[y, 1] = Convert.ToInt32(!squareValue);
+            checkers[y, 2] = Convert.ToInt32(squareValue);
+            checkers[y, 3] = Convert.ToInt32(!squareValue);
+            squareValue = !squareValue;
+            y++;
+
+            if (y > 3)
+                goto Finish; 
+
+            goto PopulateChecker;
+
+        Finish:
+            Console.WriteLine($"{checkers[0, 0]}{checkers[0, 1]}{checkers[0, 2]}{checkers[0, 3]}");
+            Console.WriteLine($"{checkers[1, 0]}{checkers[1, 1]}{checkers[1, 2]}{checkers[1, 3]}");
+            Console.WriteLine($"{checkers[2, 0]}{checkers[2, 1]}{checkers[2, 2]}{checkers[2, 3]}");
+            Console.WriteLine($"{checkers[3, 0]}{checkers[3, 1]}{checkers[3, 2]}{checkers[3, 3]}");
+
+        }
+    }
+}
